be5bde6b0d8d587669c016f024254126c487657d
Faisal <faisalm1400@gmail.com>
2025-06-01 18:44:38 +0600

    added articles

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
@@ -27,12 +27,34 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const newsCollection = client.db("newsDb").collection("articles");
+
+        app.get('/articles', async (req, res) => {
+            const result = await newsCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/articles', async (req, res) => {
+            const newArticle = req.body;
+            const result = await newsCollection.insertOne(newArticle);
+            res.send(result);
+        })
+
+        app.get('/articles/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const article = await newsCollection.findOne(query);
+            res.send(article);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
