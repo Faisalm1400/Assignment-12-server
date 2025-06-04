ff9e107d3c1c99de10bd69ab0c2c060d669fbdad
Faisal <faisalm1400@gmail.com>
2025-06-04 14:15:24 +0600

    added publishers

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 
 
@@ -32,6 +35,7 @@ async function run() {
 
         const newsCollection = client.db("newsDb").collection("articles");
         const userCollection = client.db("newsDb").collection("users");
+        const publishersCollection = client.db("newsDb").collection("publishers");
 
 
         // jwt related apis
@@ -57,14 +61,15 @@ async function run() {
             })
         }
 
-        const verifyAdmin = async(req,res, next)=>{
-            const email= req.decoded.email;
-            const query = {email: email};
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
             const user = await userCollection.findOne(query);
-            const isAdmin = user?.role=== 'admin';
-            if(!isAdmin){
-                return res.status(403).send({message: 'Forbidden  access'});
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'Forbidden  access' });
             }
+            next();
         }
 
 
@@ -101,7 +106,7 @@ async function run() {
             const id = req.params.id;
             const { title, description, status, isPremium } = req.body;
 
-            const result = await articlesCollection.updateOne(
+            const result = await newsCollection.updateOne(
                 { _id: new ObjectId(id) },
                 { $set: { title, description, status, isPremium } }
             );
@@ -120,7 +125,7 @@ async function run() {
         });
 
         // user related apis
-        app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         });
@@ -155,8 +160,8 @@ async function run() {
         app.get('/myProfile', async (req, res) => {
 
             const email = req.query.email;
-            console.log(email)
-            const result = await userCollection.find({ email }).toArray();
+            // console.log(email)
+            const result = await userCollection.findOne({ email });
             res.send(result)
         });
 
@@ -171,7 +176,7 @@ async function run() {
             res.send(result)
         });
 
-        app.patch('/users/admin/:id',verifyToken, verifyAdmin, async (req, res) => {
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
@@ -181,9 +186,21 @@ async function run() {
             }
             const result = await userCollection.updateOne(filter, updatedDoc);
             res.send(result);
-        })
+        });
 
-        app.delete('/users/:id',verifyToken, verifyAdmin, async (req, res) => {
+        app.post('/admin/publishers', async (req, res) => {
+
+            const { name, logo } = req.body;
+            if (!name || !logo) return res.status(400).json({ message: "Name and logo are required!" });
+
+            const newPublisher = { name, logo };
+            await publishersCollection.insertOne(newPublisher);
+
+            res.json({ message: "Publisher added successfully!" });
+
+        });
+
+        app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
